fc7f04a6542e95e024119c043ef849f876bd85a8
SurajPoddar02 <surajpodar20@gmail.com>
2023-04-19 03:35:55 +0530

    Initial commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
@@ -7,18 +7,39 @@ import "../src/Counter.sol";
 contract CounterTest is Test {
     Counter public counter;
 
+    // Invoked before each test
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    // Test must be external or public
+    function testInc() public {
+        counter.inc();
+        assertEq(counter.count(), 1);
     }
 
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function testFailDec() public {
+        // This will fail with underflow
+        // count = 0 --> count -= 1
+        counter.dec();
+    }
+
+    // Same as testFailDec
+    function testDecUnderflow() public {
+        vm.expectRevert(stdError.arithmeticError);
+        counter.dec();
+    }
+
+    function testDec() public {
+        counter.inc();
+        counter.inc();
+        counter.dec();
+        assertEq(counter.count(), 1);
     }
 }
+// forge build for compiling  
+// forge test for testing   
+// forge test -vvvv for detailed testing 
+// forge test --help 
+// forge test --match-path test/counter.t.sol for specific test file
+ // forge test --match path test/counter.t.sol --gas-report for gas report
